137139efc37c4a234e96c18eb2bf9eae1cb6fb23
marie-claire-yui <marieclairekasai@gmail.com>
2023-02-16 16:20:48 +0100

    a

--- script.js
+++ script.js
@@ -12,3 +12,32 @@ function getRandomInt() {
 
   
 //mybody[0].style.backgroundColor = 'rgb('+ getRandomInt() + ', '+ getRandomInt() + ', ' + getRandomInt()+ ')'; 
+
+
+
+// const body = document.getElementsByTagName('body');
+// const btn = document.getElementsById('btn');
+
+// function getRandomInt() {
+//   return Math.floor(Math.random() * 256);
+// }
+
+// function getBodyBg(){
+//   body[0].style.background = 'rgb('+getRandomInt()+','+getRandomInt()+','+getRandomInt()+')';
+// }
+
+// btn.addEventListener('click',getBodyBg);
+
+const img = document.querySelector('#img'); //sélection de l'image par son ID
+console.log(img); 
+//img.src = "https://picsum.photos/200/301";
+//img.src ="img/javascript.png";
+img.setAttribute('src','img/javascript.png');
+
+
+const div = document.getElementsByClassName("test");
+//console.log(div);
+for (let index=0; index < div.length; index++){
+ console.log(div[index].getAttribute('id'));
+}
+
